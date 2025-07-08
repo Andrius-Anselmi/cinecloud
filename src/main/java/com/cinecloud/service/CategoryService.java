777dddbf59d971a1cf6c1e2a0c1236ab5abb0525
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-07-08 20:52:03 -0300

    feat: Implementar métodos CRUD no CategoryService

---
+++ src/main/java/com/cinecloud/service/CategoryService.java
@@ -0,0 +1,44 @@
+package com.cinecloud.service;
+
+import com.cinecloud.entity.Category;
+import com.cinecloud.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Optional<Category> getById(Long id){
+        return categoryRepository.findById(id);
+    }
+
+    public List<Category> getAll(){
+        return categoryRepository.findAll();
+    }
+
+    public void deleteById(Long id){
+        categoryRepository.deleteById(id);
+    }
+
+    public Category update(Category category){
+        Optional<Category> optionalCategory = categoryRepository.findById(category.getId());
+        if(optionalCategory.isPresent()){
+            Category save = optionalCategory.get();
+            save.setId(category.getId());
+            save.setName(category.getName());
+            return categoryRepository.save(save);
+        }
+
+        return null;
+    }
+}
