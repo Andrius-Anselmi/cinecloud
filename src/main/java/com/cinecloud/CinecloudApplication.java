d1e0566094d823750d4a0a0b29c4d1167b8472c7
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-07-08 19:53:22 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/cinecloud/CinecloudApplication.java
@@ -0,0 +1,13 @@
+package com.cinecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinecloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinecloudApplication.class, args);
+    }
+
+}
