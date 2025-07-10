fae05406723bc8417804424cd01c375023bf3b79
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-07-09 21:28:39 -0300

    feat: Alterna retorno de Streaming para StreamingResponse usando o ResponseEntity

--- src/main/java/com/cinecloud/controller/StreamingController.java
+++ src/main/java/com/cinecloud/controller/StreamingController.java
@@ -1,8 +1,13 @@
 package com.cinecloud.controller;
 
+import com.cinecloud.controller.request.StreamingRequest;
+import com.cinecloud.controller.response.StreamingResponse;
 import com.cinecloud.entity.Streaming;
+import com.cinecloud.mapper.StreamingMapper;
 import com.cinecloud.service.StreamingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,38 +21,40 @@ public class StreamingController {
     private final StreamingService streamingService;
 
     @PostMapping()
-    public Streaming save(@RequestBody Streaming streaming){
-        return streamingService.save(streaming);
+    public ResponseEntity<StreamingResponse> save(@RequestBody StreamingRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).
+                body(StreamingMapper.toResponse(streamingService.save(StreamingMapper.toStreaming(request))));
     }
 
     @GetMapping()
-    public List<Streaming> findAll(){
-        return streamingService.getAll();
+    public ResponseEntity<List<StreamingResponse>>findAll(){
+      return ResponseEntity.ok(streamingService.getAll().stream()
+              .map(StreamingMapper::toResponse).toList());
     }
 
     @GetMapping("/{id}")
-    public Streaming getById(@PathVariable Long id){
-        Optional<Streaming> OptionalStreaming = streamingService.getById(id);
-        if(OptionalStreaming.isPresent()){
-            return OptionalStreaming.get();
-        }
-
-        return null;
+    public ResponseEntity<StreamingResponse> getById(@PathVariable Long id){
+        Optional<Streaming> optionalStreaming = streamingService.getById(id);
+        return optionalStreaming.map(streaming -> ResponseEntity.ok
+                (StreamingMapper.toResponse(streaming))).orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id){
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
         streamingService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
-    public Streaming update(@RequestBody  Streaming streaming, @PathVariable Long id) {
+    public ResponseEntity<StreamingResponse> update(@RequestBody  StreamingRequest request, @PathVariable Long id) {
         Optional<Streaming> OptionalStreaming = streamingService.getById(id);
         if(OptionalStreaming.isPresent()){
+            Streaming streaming = StreamingMapper.toStreaming(request);
             streaming.setId(id);
-            return  streamingService.update(streaming,id);
+            streamingService.update(streaming,id);
+            return ResponseEntity.ok(StreamingMapper.toResponse(streaming));
         }
-        return null;
+        return ResponseEntity.notFound().build();
     }
 
 
