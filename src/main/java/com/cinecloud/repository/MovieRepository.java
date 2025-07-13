52d95171d395e6f27f3b1a98531050e58e50f631
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-07-12 22:17:48 -0300

    feat: Implementa método personalizado no MovieRepository para fazer filtragem

--- src/main/java/com/cinecloud/repository/MovieRepository.java
+++ src/main/java/com/cinecloud/repository/MovieRepository.java
@@ -1,9 +1,14 @@
 package com.cinecloud.repository;
 
+import com.cinecloud.entity.Category;
 import com.cinecloud.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findByCategories(List<Category> category);
 }
